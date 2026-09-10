6af45f7b6f88a35f77c85afc031c697cdeeda189
ariba <ariba2576@gmail.com>
2026-09-10 21:48:13 +0300

    [DEV] Создать Spring Boot проект

---
+++ src/main/java/ru/bank/recommendation/BankRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.bank.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bank/recommendation/BankRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bank.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
