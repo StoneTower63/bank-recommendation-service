9d8088b97c866da73421d756c48d00f6ae9c53b9
Annie-Falcon <anna81283@mail.ru>
2026-09-14 02:33:55 +0300

    Correct issue #3

--- src/main/java/ru/bank/recommendation/model/RecommendationResponse.java
+++ src/main/java/ru/bank/recommendation/model/RecommendationResponse.java
@@ -3,11 +3,11 @@ package ru.bank.recommendation.model;
 import java.util.List;
 
 public class RecommendationResponse {
-    private String user_id;
+    private String userId;
     private List<RecommendationDto> recommendationsDTO;
 
-    public RecommendationResponse(String user_id, List<RecommendationDto> recommendationsDTO) {
-        this.user_id = user_id;
+    public RecommendationResponse(String userId, List<RecommendationDto> recommendationsDTO) {
+        this.userId = userId;
         this.recommendationsDTO = recommendationsDTO;
     }
 
@@ -15,28 +15,28 @@ public class RecommendationResponse {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         RecommendationResponse that = (RecommendationResponse) o;
-        return user_id != null ? user_id.equals(that.user_id) : that.user_id == null;
+        return userId != null ? userId.equals(that.userId) : that.userId == null;
     }
 
     @Override
     public int hashCode() {
-        return user_id != null ? user_id.hashCode() : 0;
+        return userId != null ? userId.hashCode() : 0;
     }
 
     @Override
     public String toString() {
         return "RecommendationResponse{" +
-                "user_id='" + user_id + '\'' +
+                "userId='" + userId + '\'' +
                 ", recommendationsDTO=" + recommendationsDTO +
                 '}';
     }
 
-    public String getUser_id() {
-        return user_id;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public List<RecommendationDto> getRecommendationsDTO() {
